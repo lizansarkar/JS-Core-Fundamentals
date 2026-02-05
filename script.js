b79e0e3a879f2039b9fdb8a98a8129652e37c7cf
lizan <lizansarkar16@gmail.com>
2026-02-05 16:18:18 +0600

    problem sorted

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
+// This code demonstrates JavaScript's type coercion when using the + operator with different data types.
+// problem 1
 console.log(1 + "20" + 3 + "20");
-
+// problem 2
+console.log("5" - 2);
+// The minus operator forces numeric conversion, so the string "5" is converted to a number.
+// problem 3
+// console.log("5" + 2 - 1);
+// problem 4
+// console.log(true + true + false);
+// problem 5
+// console.log([] + []);
+// problem 6
+// console.log([] + {});
+// problem 7
+// console.log(null + 1);
+// problem 8
+// console.log(undefined + 1);
+// problem 9
+// console.log("10" * "2");
+// problem 10
+// console.log(0 == false);
+// console.log(0 === false);
