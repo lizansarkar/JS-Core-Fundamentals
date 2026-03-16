2140729f97cc43f6b02cf78fc326e99064b79363
lizan <lizansarkar16@gmail.com>
2026-03-16 21:25:01 +0600

    solve five most common problem

--- thirtyJsProblem.js
+++ thirtyJsProblem.js
@@ -19,3 +19,34 @@ function isPalindrome(str) {
 }
 
 console.log(isPalindrome("madam")); // true
+
+// Problem: 3 Find the largest number in an array
+// Input: [3, 7, 2, 9, 4]
+// Expected Output: 9
+
+function findLargest(arr) {
+    // TODO
+}
+
+console.log(findLargest([3, 7, 2, 9, 4])); // 9
+
+
+// Problem: 4 Find the smallest number in an array
+// Input: [5, 1, 8, 3]
+// Expected Output: 1
+
+function findSmallest(arr) {
+    // TODO
+}
+
+console.log(findSmallest([5, 1, 8, 3])); // 1
+
+// Problem: 5 Calculate the sum of all numbers in an array
+// Input: [1, 2, 3, 4]
+// Expected Output: 10
+
+function sumArray(arr) {
+    // TODO
+}
+
+console.log(sumArray([1, 2, 3, 4])); // 10
